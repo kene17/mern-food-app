c07a1834cc85bce11cb55ecabb1dba9bf8ff91c8
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-04-02 21:23:41 -0400

    added HomePage

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './Layouts/Layout'
+import HomePage from './Pages/HomePage'
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-        <Route path="/" element={<Layout >Home Page</Layout>} />
-        <Route path="/user-profile" element={<span>Home PAge</span>} />
-        <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Layout ><HomePage /></Layout>} />
+            <Route path="/user-profile" element={<span>Home PAge</span>} />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    )
 }
 
 export default AppRoutes
