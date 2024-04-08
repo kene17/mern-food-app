7025dee1e5db0911d995cd2ae7ace54bbb75ffcc
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-04-08 19:47:01 -0400

    created user profile model on the backend

---
+++ backend/models/user.ts
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  //the below fields are not required because when the user reisters for the first time only the authid and email are provided
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
