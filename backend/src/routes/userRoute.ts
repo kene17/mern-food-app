38d207cba917577d197d4239156a28a07e3bea3a
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-04-09 00:55:45 -0400

    validate token on the backend

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import UserController from "../controller/UserController";
+import { jwtCheck } from "../middleware/auth";
 const router = express.Router();
 
-router.post("/", UserController.createCurrentUser);
+router.post("/", jwtCheck,  UserController.createCurrentUser);
 
 export default router;
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
-import {useNavigate } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 type Props = {
     children: React.ReactNode;
 }
@@ -10,18 +10,26 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience) {
         throw new Error("Unable to initlaize auth")
     }
     //handles redirect when the user comes back from successfully logging in
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
+    const onRedirectCallback = () => {
         navigate("/auth-callback")
 
     }
     return (
-        <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{ redirect_uri: redirectUri }} onRedirectCallback={onRedirectCallback}>
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+                redirect_uri: redirectUri,
+                audience
+            }}
+            onRedirectCallback={onRedirectCallback}
+        >
             {children}
         </Auth0Provider>
     )
